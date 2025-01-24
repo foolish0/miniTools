aba769a0d894d97b78dea08a6707fd81496b8b12
Gabriel <lizhenjiang.xidian@foxmail.com>
2025-01-24 10:04:36 +0800

    Gabriel - 相关配置获取

--- transformUser.py
+++ transformUser.py
@@ -1,5 +1,11 @@
 import pymysql
 import requests
+import json
+
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+    return config
 
 def get_usernames_from_db(db_config):
     conn = pymysql.connect(
@@ -14,8 +20,8 @@ def get_usernames_from_db(db_config):
     conn.close()
     return usernames
 
-def get_job_nums(usernames, access_token):
-    url = f"https://qyapi.weixin.qq.com/cgi-bin/batch/openuserid_to_userid?access_token={access_token}"
+def get_job_nums(usernames, url, access_token):
+    full_url = f"{url}?access_token={access_token}"
     headers = {
         "Content-Type": "application/json"
     }
@@ -23,20 +29,22 @@ def get_job_nums(usernames, access_token):
         "open_userid_list": usernames,
         "source_agentid": 1000212
     }
-    print(body)
-    response = requests.post(url, json=body, headers=headers)
+    response = requests.post(full_url, json=body, headers=headers)
     data = response.json()
-    print(data)
 
     if data["errcode"] != 0:
         raise Exception(f"Error: {data['errmsg']}")
 
     return {item["open_userid"]: item["userid"] for item in data["userid_list"]}
 
-def generate_and_execute_update_statements(db_config, access_token):
+def generate_and_execute_update_statements(config_path):
+    config = load_config(config_path)
+    db_config = config['db_config']
+    url = config['url']
+    access_token = config['access_token']
+
     usernames = get_usernames_from_db(db_config)
-    print(usernames)
-    job_nums = get_job_nums(usernames, access_token)
+    job_nums = get_job_nums(usernames, url, access_token)
     update_statements = []
 
     conn = pymysql.connect(
@@ -78,11 +86,5 @@ def generate_and_execute_update_statements(db_config, access_token):
     return update_statements
 
 # 示例使用
-db_config = {
-    'host': '192.168.4.80',
-    'user': 'root',
-    'password': '4DrveSG!jTu%',
-    'database': 'data_warehouse_prod'
-}
-access_token = 'j9UB-RKQNcqB95S92u8sPIuQBQt8yEneFB69J0sNfW0lbMqIrExikSNXtauCW4dYHroCYZSX4Y-42w0lN4zGoUPgZBoDz2iz9ahYSA8RXprkD2zClwTcIYN-R8kdKZncNoXmZFhDhMk2g0y8CkJoqjt0iY8aQOODDQ8lqc_VZ1aqkkmkBVk7GHXPNrCZnbNWAGpU0NxyXevCWLW5YiQf9w'
-generate_and_execute_update_statements(db_config, access_token)
+config_path = 'config/config.json'
+generate_and_execute_update_statements(config_path)
